39e3b12e414180fed0b7ebdda017258a5f90bc1f
rr-phones <59873802+rr-phones@users.noreply.github.com>
2020-01-30 09:10:27 -0300

    z

---
+++ fase03/ex03/vogalOuConsoante.js
@@ -0,0 +1,3 @@
+function vogalOuConsoante(letra){
+    var letra = letra.toLowerCase()
+    if(letra === 'a' || letra === 'e'|| letra === 'i' || letra === 'o' || letra === 'u'){return 'Vogal'}else{return 'Consoante'}}
