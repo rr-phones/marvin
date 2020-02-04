8a971d2d7e74403384ff9b9e4214a095f3dc9677
rr-phones <59873802+rr-phones@users.noreply.github.com>
2020-02-04 09:51:23 -0300

    az

--- fase03/ex00/parOuImpar.js
+++ fase03/ex00/parOuImpar.js
@@ -1,3 +1,9 @@
-function nove(numero){
-    return numero.toString().substr(-1, 1)==9
+function parOuImpar(numero){
+    var numero = (numero)
+    var total = numero % 2;
+if (total == 0){
+    return ("par")
+}else{
+    return ("impar")
+}
 }
