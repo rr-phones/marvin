d76a89cb2cbff3544d128db9a53878002e0c3e14
rr-phones <59873802+rr-phones@users.noreply.github.com>
2020-02-06 11:15:22 -0300

    bbf

---
+++ fase04/ex01/ateZero.js
@@ -0,0 +1,13 @@
+function ateZero (numero){
+    var n = 0
+    while (n <= numero){
+        console.log(n)
+        n++
+    }
+    if(numero <= 0){
+    while(n >= numero){
+        console.log(numero)
+        numero++
+    }
+  }
+}
