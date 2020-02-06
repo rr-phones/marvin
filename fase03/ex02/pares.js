a9733d85123d0374ed2bbf7861e11ab3324e41ae
rr-phones <59873802+rr-phones@users.noreply.github.com>
2020-02-06 09:19:16 -0300

    zuzum

--- fase03/ex02/pares.js
+++ fase03/ex02/pares.js
@@ -1,9 +1,11 @@
-function pares( num ){
-var n = num;
-if(num % 2 == 0){
-    return( "os numeros sào pares" )
-}
-else{
-    return("os numeros nao sào pares");
-}
+function parOuImpar(num1, num2){
+    var resto1 = num1 % 2;
+    var resto2 = num2 % 2;
+    var total = resto1 || resto2 ;
+    
+    if(total === 0){
+        return("Os números são pares")
+    }else {
+        return("Os números não são pares")
+    }
 }
