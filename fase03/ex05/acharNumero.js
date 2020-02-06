5737e28629571b1b0419d5bd823ccf7702f46361
rr-phones <59873802+rr-phones@users.noreply.github.com>
2020-02-06 08:44:31 -0300

    lulu

---
+++ fase03/ex05/acharNumero.js
@@ -0,0 +1,20 @@
+function acharNumero(numero){
+    var r = numero 
+    
+    if(r < 5){
+    return ("mercurio")
+    
+    }if (r < 11){
+
+    return ("venus")
+    
+}
+    if(r  < 101 ){
+        
+    return ("terra")
+    
+    }if (r > 100)
+    return("marte")
+}
+
+
