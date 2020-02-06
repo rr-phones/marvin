7aaf47df0a9688bcc8d986742d5405d0b88fdb35
rr-phones <59873802+rr-phones@users.noreply.github.com>
2020-02-06 11:51:33 -0300

    kk

---
+++ fase04/ex02/fizzbuzz.js
@@ -0,0 +1,19 @@
+function fizzbuzz(num1, num2){
+   var a = num1
+   while (a<= num2){
+        if ( a % 3 === 0 && a % 5 === 0){
+          console.log(a + " Fizzbuzz ")
+          a ++ 
+        }
+        if(a % 5 === 0){
+          console.log(a + " Buzz ")
+          a++
+    }
+        if(a % 3 === 0){
+          console.log(a + " Fizz ")
+          a++
+}
+console.log(a)
+a++
+}
+}
