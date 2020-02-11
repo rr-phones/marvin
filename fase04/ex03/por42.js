dad569c33207bc9fe1070afa77bacaa4c413bdcf
rr-phones <59873802+rr-phones@users.noreply.github.com>
2020-02-11 11:02:40 -0300

    nn

---
+++ fase04/ex03/por42.js
@@ -0,0 +1,23 @@
+function por42(num1, num2){
+	var mult = 42
+  var cont = 2
+  var flag = 0
+  var result = null;
+  while (mult < num2){
+  	if(mult > num1){
+      
+      if (flag == 1) {
+					result = mult
+      } else {
+				flag = 1
+      } 
+  	} 
+    mult = 42 * cont
+    cont++
+  }
+  if (!result) {
+  	console.log('Não Encontrado');
+    return false;
+  } 
+  return result;
+}
