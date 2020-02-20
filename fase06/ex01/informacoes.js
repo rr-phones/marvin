d468d45b2bc06fad83d8fa74cee4fd9a80ef1ec8
rr-phones <59873802+rr-phones@users.noreply.github.com>
2020-02-20 11:38:06 -0300

    cgrvgt

--- fase06/ex01/informacoes.js
+++ fase06/ex01/informacoes.js
@@ -1,15 +1,3 @@
-function animais (array) {  
-    var  obj = array;
-    var novaIdade = 5;
-    var i = 0;
-   
-    while (i< array.length) {
-        var posicao = obj[i];
-        delete posicao.altura;
-        if (posicao.idade !== null) {
-            posicao.idade = novaIdade}
-            novaIdade++;
-            i++;
-        }
-    return array;
+function informacoes (obj, prop) {
+    return obj[prop] || false;
 }
